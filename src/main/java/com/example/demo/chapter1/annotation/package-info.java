fd9d220e89fa8e8f8681abff4d688c523a3f50ca
lipingan <1737349741@qq.com>
2018-01-25 19:41:05 +0800

    添加包注释

---
+++ src/main/java/com/example/demo/chapter1/annotation/package-info.java
@@ -0,0 +1,18 @@
+
+/**
+ * 此包主要学习声明Bean的注解
+ * 
+ * @Service
+ * @Repository
+ * @Component
+ * @Controller
+ * @RestController
+ * @RequestMapping
+ * @ResponseBody
+ * @PathVariable
+ * @Scope
+ * 
+ * @author lpa
+ *
+ */
+package com.example.demo.chapter1.annotation;
---
+++ src/main/java/com/example/demo/chapter1/useannotation/package-info.java
@@ -0,0 +1,12 @@
+
+/**
+ * 此包主要学习注入Bean的注解
+ * 
+ * @Autowired
+ * @Inject
+ * @Resource
+ * 
+ * @author Administrator
+ *
+ */
+package com.example.demo.chapter1.useannotation;
